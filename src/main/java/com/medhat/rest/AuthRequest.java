375bd6e4c1ccf00bf2db1a35da917624fb1caa66
MedFadl <mohamedmedfadl@gmail.com>
2026-07-30 17:05:27 +0300

    added Auth request model

---
+++ src/main/java/com/medhat/rest/AuthRequest.java
@@ -0,0 +1,51 @@
+package com.medhat.rest;
+
+public class AuthRequest {
+
+    private Long id;
+    private String employeeName;
+    private String requestDetails;
+    private RequestStatus status;
+
+    public AuthRequest() {
+    }
+
+    public AuthRequest(Long id, String employeeName, String requestDetails, RequestStatus status) {
+        this.id = id;
+        this.employeeName = employeeName;
+        this.requestDetails = requestDetails;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getRequestDetails() {
+        return requestDetails;
+    }
+
+    public void setRequestDetails(String requestDetails) {
+        this.requestDetails = requestDetails;
+    }
+
+    public RequestStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(RequestStatus status) {
+        this.status = status;
+    }
+}
