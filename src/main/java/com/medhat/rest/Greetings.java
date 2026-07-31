5a20230600b2c45b21dd238237ca1bea35947954
MedFadl <mohamedmedfadl@gmail.com>
2026-07-31 20:52:42 +0300

    Added greetings per role based on keycloak JWT TOKEN

--- src/main/java/com/medhat/rest/Greetings.java
+++ src/main/java/com/medhat/rest/Greetings.java
@@ -5,7 +5,9 @@ import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path( "/")
 public class Greetings {
@@ -14,7 +16,7 @@ public class Greetings {
     @GET
     @Path("/employee")
     @Produces(MediaType.TEXT_PLAIN)
-    @RolesAllowed(value = { "employee" })
+    @RolesAllowed(value = { "Employee" })
     public String sayHelloToEmployee(){
         return "Hello Em POH! LEE!!HHH >:c";
     }
@@ -24,6 +26,24 @@ public class Greetings {
     @Produces(MediaType.TEXT_PLAIN)
     @RolesAllowed(value = { "Manager" })
     public String sayHelloToManager(){
-        return "Hello Mana gar :D";
+        return "Hello Manaaaaaaaaaaaaaaaa gaaaaaaaaaaaar :D";
+    }
+
+    //Something better c:
+    @GET
+    @Path("/dashboard")
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed(value = { "Employee", "Manager" })
+    public String sayHelloToDashboard(@Context SecurityContext SecurityContext){
+        if(SecurityContext.isUserInRole("Employee")){
+            return "Welcome to your dashboard MR EM POOOH LEEEH >:C";
+        }
+        else if(SecurityContext.isUserInRole("Manager")){
+            return "Welcome to your dashboard MANAGER :D <3";
+        }
+        else{
+            return "You are not authorized to access this page";
+        }
+
     }
 }
